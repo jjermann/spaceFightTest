aee5f83466b64ada6f06f1ab1bbecd7daa9fe390
Jonas Jermann <jjermann2@gmail.com>
2018-04-29 20:26:13 +0200

    add canvas example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Canvas from './components/Canvas';
 import PropTypes from 'prop-types';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{this.props.message}</h1>
+        <Canvas />
       </div>
     );
   }
